80ba58ec2e4233b51ce673aa25a14ef401e8efea
coalexe <alex.cote@gmail.com>
2017-01-03 12:51:30 -0500

    Corrected asset path

--- src/NotificationsAsset.php
+++ src/NotificationsAsset.php
@@ -14,12 +14,12 @@ class NotificationsAsset extends AssetBundle
   public $sourcePath = '@vendor';
   public $css = [
     '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-    'coalexe/src/css/notification.css'
+    'coalexe/notifications/src/css/notifications.css'
   ];
   public $js = [
-    'coalexe/src/js/notifications.js'
+    'coalexe/notifications/src/js/notifications.js'
   ];
   public $depends = [
-    'WidgetFactoryAsset'
+    'coalexe\notifications\WidgetFactoryAsset'
   ];
 }
